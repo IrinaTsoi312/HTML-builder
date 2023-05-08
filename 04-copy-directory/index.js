d768b0b3dfd9f520334e50dcfc8220ce0ab3ee65
Irina Tsoi <irinatsoi312@gmail.com>
2023-05-09 02:54:14 +0600

    feat: add solution to 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
+const origDir = path.join(__dirname, 'files');
+const copyDir = path.join(__dirname, 'files-copy');
+
+async function copyFiles() {
+  try {
+    const files = await fs.readdir(origDir);
+    for (const file of files) {
+      fs.copyFile(`${origDir}/${file}`, `${copyDir}/${file}`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function copyFolder() {
+  fs.mkdir(copyDir, (err) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      console.log('Folder was created');
+    }
+  });
+  copyFiles();
+}
+
+async function haveFolder() {
+  const direct = await fs.readdir(__dirname);
+  if (direct.includes('files-copy')) {
+    fs.rm(copyDir, {recursive: true}, (err) => {
+      if (err) {
+        return console.error('Error');
+      }
+    });
+  } 
+  copyFolder();
+}
+haveFolder();
